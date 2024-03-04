39cc3145787957eb5d15f3134fbea04152fd1781
Alex <103133406+AlexanderKudr@users.noreply.github.com>
2024-03-04 23:47:53 +0300

    Update ts-notes.ts

--- ts-notes.ts
+++ ts-notes.ts
@@ -26,3 +26,27 @@ newobj1.bro = 2;
 newobj2.bro = 3;
 console.log(obj1);
 console.log(obj2);
+
+
+//
+// here is the function to check type of the input in JS
+const objectToString = Object.prototype.toString;
+const trueTypeOf = (input) => objectToString.call(input);
+
+// what type is this?
+const obj = {};
+const array = [];
+const func = function () {};
+const undef = undefined;
+const nullish = null;
+
+// here is the comparison to typeof, trueTypeOf is more advanced
+console.log(trueTypeOf(obj), typeof obj, obj instanceof Object);
+console.log(trueTypeOf(array), typeof array, array instanceof Array);
+console.log(trueTypeOf(func), typeof func, func instanceof Function);
+console.log(trueTypeOf(undef), typeof undef, undef instanceof Object);
+console.log(trueTypeOf(nullish), typeof nullish, nullish instanceof Object);
+
+// source
+("https://javascript.info/instanceof");
+
